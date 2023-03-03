de717bd6ad94f1bcfdca8cb3ac08a415bf2c76a0
Josh Blake <67488691+2003jblake@users.noreply.github.com>
2023-03-03 17:58:51 +0000

    added find start node and find end node, also started work on depth first search

--- mazesolver.py
+++ mazesolver.py
@@ -1,14 +1,41 @@
-def readMaze(filepath):
-    matrixArray = []
+'''This module defines the functions used to solve the mazes given using a varity of algorithms'''
 
-    with open(filepath) as file:
+def readMaze(filepath):
+    '''This takes in a filepath to a maze.
+    Returns a maze where each row is a list item in an array
+    Removes any newline characters and empty lines'''
+
+    matrix_array = []
+
+    with open(filepath, encoding="utf-8") as file:
         for item in file:
             if item == '\n':
                 break
-            matrixArray.append(item.replace("\n", ""))
+            matrix_array.append(list(item.replace('\n', '').replace(' ', '')))
+
+    return matrix_array
+
+def findStart(matrix_array):
+    '''This returns the x index of the start node of the array given that 
+    the start is always on the top row'''
+
+    for index, char in enumerate(matrix_array[0]):
+
+        if char == '-':
+            return index
+
+    return -1
+
+def findEnd(matrix_array):
+    '''This returns the x index of the start node of the array given that
+    the start is always on the bottom row'''
+
+    for index, char in enumerate(matrix_array[-1]):
+        if char == '-':
+            return index
+
+    return -1
 
 
-    return matrixArray
-
-
-print(readMaze("maze-Easy.txt"))
+a = readMaze("maze-Easy.txt")
+print(findStart(a))
