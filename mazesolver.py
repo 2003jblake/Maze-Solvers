a9ccd4341e4fab47c93e5c2c8023f04e9c629c15
Josh Blake <67488691+2003jblake@users.noreply.github.com>
2023-03-04 17:47:27 +0000

    added depth first function that can solve mazes with no cycles or dead ends

--- mazesolver.py
+++ mazesolver.py
@@ -1,6 +1,6 @@
 '''This module defines the functions used to solve the mazes given using a varity of algorithms'''
 
-def readMaze(filepath):
+def read_maze(filepath):
     '''This takes in a filepath to a maze.
     Returns a maze where each row is a list item in an array
     Removes any newline characters and empty lines'''
@@ -15,7 +15,7 @@ def readMaze(filepath):
 
     return matrix_array
 
-def findStart(matrix_array):
+def find_start(matrix_array):
     '''This returns the x index of the start node of the array given that 
     the start is always on the top row'''
 
@@ -26,7 +26,7 @@ def findStart(matrix_array):
 
     return -1
 
-def findEnd(matrix_array):
+def find_end(matrix_array):
     '''This returns the x index of the start node of the array given that
     the start is always on the bottom row'''
 
@@ -37,5 +37,69 @@ def findEnd(matrix_array):
     return -1
 
 
-a = readMaze("maze-Easy.txt")
-print(findStart(a))
+def  depth_first_search(matrix_array):
+    '''This peforms depth first search'''
+
+    def valid_move(destinationCords):
+        '''This checks if is a move is valid based on the cordinates of the wanted move.
+        takes in a tuple of two coordinates and returns true if move is valid false otherwise'''
+
+        if destinationCords[0] < 0 or destinationCords[1] < 0:
+            return False
+        if destinationCords[0] > (len(matrix_array[0])-1) or destinationCords[1] > (len(matrix_array)-1):
+            return False
+
+        if matrix_array[destinationCords[1]][destinationCords[0]] == '-':
+            return True
+
+        return False
+
+    def check_cycle(destinationCords):
+
+        for item in routeStack:
+            if destinationCords == item:
+                return False
+
+        return True
+
+
+    start = find_start(matrix_array)
+    end = find_end(matrix_array)
+    if start == -1 or end == -1:
+        print("Maze is invalid, the start must be in the top row and the end must be bottom row")
+
+    routeStack = []
+    routeStack.append([start, 0])
+
+    routeFound = False
+
+    while not routeFound:
+
+        position = routeStack[-1]
+
+        if position == [end, len(matrix_array)-1]:
+            routeFound = True
+            return routeStack
+
+        if valid_move([position[0], position[1]-1]) and check_cycle([position[0], position[1]-1]):
+            routeStack.append([position[0], position[1]-1])
+            
+        elif valid_move([position[0]-1, position[1]]) and check_cycle([position[0]-1, position[1]]):
+            routeStack.append([position[0]-1, position[1]])
+
+        elif valid_move([position[0], position[1]+1]) and check_cycle([position[0], position[1]+1]):
+            routeStack.append([position[0], position[1]+1])
+            
+        elif valid_move([position[0]+1, position[1]]) and check_cycle([position[0]+1, position[1]]):
+            routeStack.append([position[0]+1, position[1]])
+
+        
+
+
+
+
+
+
+
+a = read_maze("test.txt")
+print(depth_first_search(a))
